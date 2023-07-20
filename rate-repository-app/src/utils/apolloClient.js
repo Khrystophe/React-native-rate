6a10f4b0bb2a3175971129756cb531555f235681
Christophe Meckler <christophe.meckler@stagiairesmns.fr>
2023-07-20 13:13:36 +0200

    m

--- rate-repository-app/src/utils/apolloClient.js
+++ rate-repository-app/src/utils/apolloClient.js
@@ -2,10 +2,10 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import Constants from "expo-constants";
 import { setContext } from "@apollo/client/link/context";
 
-// const { apollo_uri } = Constants.manifest.extra;
+const { apollo_uri } = Constants.manifest.extra;
 
 const httpLink = createHttpLink({
-  uri: "http://172.16.1.36:4000/graphl",
+  uri: apollo_uri,
 });
 
 const createApolloClient = (authStorage) => {
